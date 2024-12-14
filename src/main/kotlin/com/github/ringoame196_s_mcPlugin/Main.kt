c1253c164cce5615d2ddb5ab7f189bb289071b99
りんご飴196 <kazumac41@gmail.com>
2024-12-14 21:08:39 +0900

    ✨  :config関係

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -2,7 +2,8 @@ package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.TabCompleter
-import com.github.ringoame196_s_mcPlugin.events.Events
+import com.github.ringoame196_s_mcPlugin.events.EntityDamageByEntityEvent
+import com.github.ringoame196_s_mcPlugin.events.PlayerInteractEntityEvent
 import com.github.ringoame196_s_mcPlugin.managers.DataManager
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -17,10 +18,17 @@ class Main : JavaPlugin() {
         saveResource("cpsgame_data.db", false)
         DataManager.registerTableFileName("${plugin.dataFolder.path}/cpsgame_data.db")
 
-        // マイクライベント コマンド関係
-        server.pluginManager.registerEvents(Events(), plugin)
+        // config
+        Data.limitTime = config.getInt("limit_time")
+
+        // マイクライベント
+        server.pluginManager.registerEvents(PlayerInteractEntityEvent(), plugin)
+        server.pluginManager.registerEvents(EntityDamageByEntityEvent(), plugin)
         val command = getCommand("cpsgame")
         command!!.setExecutor(Command())
         command.tabCompleter = TabCompleter()
+
+        // ゲーム関係
+        GameTimer.runTaskTimer(plugin, 0L, 20L)
     }
 }
